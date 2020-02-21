5709b1ca3cbfb6ff7112c0afc27dc0aae1c17e4a
AntonKalaturnyi <kalaturny@gmail.com>
2020-02-22 01:44:58 +0200

    PassportService/Controller.updateBorrowerPass(dto) works

--- src/main/java/com/growit/api/controller/PassportController.java
+++ src/main/java/com/growit/api/controller/PassportController.java
@@ -3,6 +3,7 @@ package com.growit.api.controller;
 import com.growit.api.domain.CreditCard;
 import com.growit.api.domain.Passport;
 import com.growit.api.dto.BorrowerPassportDto;
+import com.growit.api.dto.Existing;
 import com.growit.api.dto.InvestorPassportDto;
 import com.growit.api.dto.New;
 import com.growit.api.service.PassportService;
@@ -13,7 +14,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/card")
+@RequestMapping("/passport")
 public class PassportController {
 
     private final PassportService passportService;
@@ -24,14 +25,20 @@ public class PassportController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(value = "/new-borrower-passport", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public Passport setBorrowerPassport(@Validated(New.class) @RequestBody BorrowerPassportDto card) {
-        return passportService.createBorrowerPass(card);
+    public Passport setBorrowerPassport(@Validated(New.class) @RequestBody BorrowerPassportDto dto) {
+        return passportService.createBorrowerPass(dto);
+    }
+
+    @ResponseStatus(HttpStatus.OK)
+    @PutMapping(value = "/update-borrower-passport", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Passport updateBorrowerPassport(@Validated(Existing.class) @RequestBody BorrowerPassportDto dto) {
+        return passportService.updateBorrowerPass(dto);
     }
 
     @PreAuthorize("hasAuthority('REGISTERED_USER')")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(value = "/new-investor-passport", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public InvestorPassportDto setInvestorPassport(@Validated(New.class) @RequestBody InvestorPassportDto card) {
-        return passportService.createInvestorPass(card);
+    public InvestorPassportDto setInvestorPassport(@Validated(New.class) @RequestBody InvestorPassportDto dto) {
+        return passportService.createInvestorPass(dto);
     }
 }
