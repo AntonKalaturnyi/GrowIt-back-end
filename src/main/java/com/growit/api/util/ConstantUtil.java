f3214295eddea87c9fec300c7ce78ada4c8c61f6
Anton Kalaturnyi <31220393+AntonKalaturnyi@users.noreply.github.com>
2020-03-13 22:05:52 +0200

    Investor registration part I with SMS code sending to front-end works
    * PassportService/Controller.updateBorrowerPass(dto) works
    
    * Address moved from User to Borrower
    
    * InvestorAccount: late days changed
    
    * cors fixed
    
    * Investor registration part I with SMS code sending to front-end works

--- src/main/java/com/growit/api/util/ConstantUtil.java
+++ src/main/java/com/growit/api/util/ConstantUtil.java
@@ -4,6 +4,8 @@ public class ConstantUtil {
 
     public static final String VALID_POSTAL_CODE_REGEXP = "\\d{5}";
 
+    public static final String VALID_PHONE = "^\\+?3?8?(0[5-9][0-9]\\d{7})$";
+
     public static final String LAND_UA = "ua";
 
     public static final String LAND_ENG = "eng";
