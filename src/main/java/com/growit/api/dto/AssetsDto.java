42dc5e01e7ccde3215767cf62ff6de54742ad00f
Anton Kalaturnyi <31220393+AntonKalaturnyi@users.noreply.github.com>
2020-04-10 13:52:15 +0300

    getAssetsData() endpoint added for input load in assets-fill-form
    * PassportService/Controller.updateBorrowerPass(dto) works
    
    * Address moved from User to Borrower
    
    * InvestorAccount: late days changed
    
    * cors fixed
    
    * Investor registration part I with SMS code sending to front-end works
    
    * saveInvestorPassportAndItn() endpoint added to Investor controller
    
    * Borrower registration from creds fixed
    
    * Borrower registration part I with SMS code sending to front-end works
    
    * borrower passport/itn/photoWithPassport endpoint works
    
    * borrower saveLivingAddress() endpoint works
    
    * saveEmploymentInfo() rough works/Employer refactored to Employment
    
    * Loan creation from calculator fields added
    
    * description added to Loan creation from calculator
    
    * saveEducationInfo() endpoint added for BorrowerControlle
    
    * file size/issueDate/AuthenticationPrincipal fixes added to Passport filling
    
    * pictures removed from uploads
    
    * assets endpoint added + fields to Borrower
    
    * borrower/set-employment improved +SocilaStatus added
    
    * BorrowerController.getRegistrationData() GET endpoint added
    
    * BorrowerConttoller.getDocumentsData() added
    
    * BorrowerController.
    
    * BorrowerController.getEmploymentData() GET endpoint added
    
    * BorrowerController.getEducationData() GET endpoint added
    
    * DashboardLoanDto added, getDashboardLoanList() returns simple list of loans
    
    * getDashboardLoanList() timeLeft added
    
    * DashboardLoanDto: fulfillment % added
    
    * CreditHistory refactored; getDashboardLoanList() returns loans with expandable details
    
    * makeInvestments() works
    
    * fixed error on loans load on null released_date
    
    * ContactPersons support added for fill employment page
    
    * loan amountFunded filling fixed in service
    
    * getAssetsData() endpoint added for input load in assets-fill-form

--- src/main/java/com/growit/api/dto/AssetsDto.java
+++ src/main/java/com/growit/api/dto/AssetsDto.java
@@ -14,4 +14,12 @@ public class AssetsDto extends AbstractDto {
     private boolean hasCar;
     private String carAge;
     private boolean wasAbroad;
+
+    public AssetsDto(String flat, boolean hasHouse, boolean hasCar, String carAge, boolean wasAbroad) {
+        this.flat = flat;
+        this.hasHouse = hasHouse;
+        this.hasCar = hasCar;
+        this.carAge = carAge;
+        this.wasAbroad = wasAbroad;
+    }
 }
