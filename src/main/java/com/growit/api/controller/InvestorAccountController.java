8c3e7e4c2802bf795ee73d3aedd4671416932d4c
Anton Kalaturnyi <31220393+AntonKalaturnyi@users.noreply.github.com>
2020-05-10 23:39:19 +0300

    InvestorCabinet getData() added
    * can delere loan on funding from cabinet
    
    * InvestorCabinet getData() added

--- src/main/java/com/growit/api/controller/InvestorAccountController.java
+++ src/main/java/com/growit/api/controller/InvestorAccountController.java
@@ -1,11 +1,13 @@
 package com.growit.api.controller;
 
+import com.growit.api.domain.Investor;
 import com.growit.api.dto.Existing;
 import com.growit.api.dto.InvestorAccountDto;
 import com.growit.api.service.InvestorAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -20,10 +22,16 @@ public class InvestorAccountController {
         this.investorAccountService = investorAccountService;
     }
 
-    /** id of InvestorAccount must be privided in dto*/
+/*    *//** id of InvestorAccount must be provided in dto*//*
     @ResponseStatus(HttpStatus.OK)
     @PutMapping(value = "/update", consumes = MediaType.APPLICATION_JSON_VALUE)
     public InvestorAccountDto update(@Validated(Existing.class) @RequestBody InvestorAccountDto dto) {
         return investorAccountService.update(dto);
+    }*/
+
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(value = "/get-data", produces = MediaType.APPLICATION_JSON_VALUE)
+    public InvestorAccountDto getGeneralData(@AuthenticationPrincipal Investor investor) {
+        return investorAccountService.getAccountData(investor);
     }
 }
--- src/main/java/com/growit/api/service/InvestorAccountService.java
+++ src/main/java/com/growit/api/service/InvestorAccountService.java
@@ -1,27 +1,37 @@
 package com.growit.api.service;
 
+import com.growit.api.domain.Investor;
+import com.growit.api.domain.InvestorAccount;
+import com.growit.api.domain.Role;
 import com.growit.api.dto.InvestorAccountDto;
-import com.growit.api.mapper.InvestorAccountMapper;
 import com.growit.api.repo.InvestorAccountRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class InvestorAccountService {
 
     private final InvestorAccountRepo investorAccountRepo;
-    private final InvestorAccountMapper mapper;
 
     @Autowired
-    public InvestorAccountService(InvestorAccountRepo investorAccountRepo, InvestorAccountMapper mapper) {
+    public InvestorAccountService(InvestorAccountRepo investorAccountRepo) {
         this.investorAccountRepo = investorAccountRepo;
-        this.mapper = mapper;
     }
 
+/*
     public InvestorAccountDto update(InvestorAccountDto dto) {
         dto.setCreated(investorAccountRepo.findById(dto.getId()).get().getCreated());
         return mapper.toDto(investorAccountRepo.save(mapper.toEntity(dto)) );
     }
+*/
 
-
+    @Transactional
+    @PreAuthorize("hasAnyAuthority('REGISTERED_INVESTOR')")
+    public InvestorAccountDto getAccountData(Investor investor) {
+        InvestorAccount account = investor.getAccount();
+        return investor.getRoles().contains(Role.INVESTOR) ? new InvestorAccountDto(account.getAvailableBalance(), account.getInvestedFunds()) :
+                new InvestorAccountDto();
+    }
 }
